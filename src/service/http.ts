2a1de8233595ccb86317a460e5e076db838d2364
zhanghaiyan <zhanghaiyan@huobi.com>
2023-04-18 16:06:10 +0800

    feat: 封装请求函数

---
+++ src/service/http.ts
@@ -0,0 +1,109 @@
+import axios from 'axios';
+// 先简单获取下语言
+const getLang = () => {
+    let lang = location.pathname.split('/')[1] || 'en-us';
+    const items = lang.split('-');
+    return `${items[0]}-${items[1].toUpperCase()}`;
+};
+
+/**
+ * 错误处理方法
+ *
+ * @param {Object} err 错误对象
+ * @return {Object}
+ */
+const error = (err:any) => ({
+    success: false,
+    code: 500,
+    message: '访问出错，请稍后重试',
+    data: null,
+    trace: err.stack ? err.stack : err,
+});
+
+/**
+ * 格式化响应数据
+ *
+ * @param {Object}
+ * @return {Object} 返回响应数据结果对象
+**/
+const transformResponse = (res: any) => {
+    const data = res.request && res.config ? res.data : res;
+
+    // 如果不是正常 JSON 数据则认为是错误
+    if (typeof data !== 'object' || Array.isArray(data)) {
+        return error('数据响应格式不正确。');
+    }
+    if (data.code === 5005) {
+        sessionStorage.setItem('tokenExpire', '0');
+    }
+    // 封装后端 Response Schema
+    // 具体需要根据后端而定
+    data.success = typeof data.success === 'boolean'
+        ? data.success
+        : (data.success || data.status === 'ok' || data.code === 200 || data.code === 0);
+    data.code = data.code || data['err-code'] || 200;
+    data.message = data.message || data['err-msg'] || 200;
+
+    return data;
+};
+
+
+// 请求参数处理
+axios.interceptors.request.use((config:any) => {
+    // eslint-disable-next-line
+  config.params = {
+        ...config.params,
+    };
+
+    // eslint-disable-next-line
+  config.headers = {
+        'Accept-Language': getLang(),
+        'Content-Type': 'application/json',
+        'Admin-Token': localStorage.getItem('Admin-Token') || '',
+        ...config.headers,
+    };
+    return config;
+}, (error: any) => Promise.reject(error));
+
+
+// 响应拦截器
+axios.interceptors.response.use((res: any) => transformResponse(res), (err:any) => error(err));
+
+export class Request {
+  /**
+   * get方法
+   *
+   * @param {string} url 路径
+   * @param {Object} params 参数
+   * @param headers
+   */
+  static get = (url: string, params?: any, headers: Record<string, any> = {}) => {
+      const path = window.location.pathname.split('/');
+      const lang = path[1];
+      return new Promise((resolve, reject) => {
+          axios
+              .get(url, { params })
+              .then((res:any) => {
+                  resolve(res);
+              })
+              .catch((err: any) => {
+                  reject(err);
+              });
+      });
+  };
+
+  static post = (url: string, params?: any, headers: Record<string, any> = {}) => {
+      const path = window.location.pathname.split('/');
+      const lang = path[1];
+      return new Promise((resolve, reject) => {
+          axios
+              .post(url, { ...params }, headers)
+              .then((res: any) => {
+                  resolve(res);
+              })
+              .catch((err: any) => {
+                  reject(err);
+              });
+      });
+  };
+}
--- src/views/im/components/ChatWindow.vue
+++ src/views/im/components/ChatWindow.vue
@@ -1,10 +1,36 @@
 <template>
-    <div>dfsfsf</div>
+  <div>dfsfsf</div>
 </template>
 
 <script>
+import TIM from 'tim-js-sdk';
+import TIMUploadPlugin from 'tim-upload-plugin';
+import TIMProfanityFilterPlugin from 'tim-profanity-filter-plugin';
+import { reactive, toRefs } from 'vue';
+import { getTimAppId } from '@/api/im';
+
+
 export default {
-name: "ChatWindow"
+  name: "ChatWindow",
+  setup() {
+    const state = reactive({
+      SDKAppID: null, // 接入时需要将0替换为您的即时通信 IM 应用的 SDKAppID
+    });
+
+    /**
+     * 请求后端的接口进行获取IM的的appId
+     * */
+    const getIMAppId = async () => {
+      debugger
+      const res = await getTimAppId();
+      state.SDKAppID = res.data || null;
+    }
+    getIMAppId();
+
+    return {
+      ...toRefs(state),
+    }
+  },
 }
 </script>
 
