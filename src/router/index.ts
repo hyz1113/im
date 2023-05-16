23210f363f15ba7b0c3986db915fc64bdc6a6005
zhanghaiyan <zhanghaiyan@huobi.com>
2023-05-16 17:46:36 +0800

    test: 添加 log 3

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,17 +3,17 @@ import messageList from '@/views/im/messageList.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: 'list',
+    path: '/list',
     name: 'list',
     component: messageList
   },
   {
-    path: 'chat',
+    path: '/chat',
     name: 'chat',
     component: import('@/views/im/chat.vue')
   },
   {
-    path: 'notice',
+    path: '/notice',
     name: 'notice',
     component: import('@/views/im/msgNotice.vue')
   },
