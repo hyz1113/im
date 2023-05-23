be4c7e252ee3f160a7f91e5d1c54086a3f09201f
zhanghaiyan <zhanghaiyan@huobi.com>
2023-05-23 19:44:29 +0800

    fix: 添加文件

---
+++ src/views/im/components/scrollBottom.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content">
+      <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+     window.onscroll = this.handleScroll;
+  },
+   methods: {
+     handleScroll() {
+       // 获取滚动的距离
+       let scrollTop=document.documentElement.scrollTop;
+       if(scrollTop === 0){
+         console.log("=========我到顶了============");
+         this.$emit('scrollTopGetList');
+       }
+     },
+   },
+}
+</script>
+
+<style scoped lang="scss">
+.content {
+  overflow: scroll;
+}
+</style>
